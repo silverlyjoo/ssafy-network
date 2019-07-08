0c11495f066f46e29e0e0cb4e7bbed07612a9e54
PSMiswell <smp19910413@gmail.com>
2019-07-08 15:37:56 +0900

    [박성민 브랜치 따기 전] 모르고 진행, push 하고 브랜치 파서 작업하겠음

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import AboutUs from '@/components/common/AboutUs.vue'
 import Post from '@/views/PostPage.vue'
 import Portfolio from '@/views/PortfolioPage.vue'
 import Project from '@/views/ProjectPage.vue'
+import PortfolioWriter from '@/views/PortfolioWriter'
 
 Vue.use(Router)
 
@@ -36,6 +37,11 @@ export default new Router({
       path: '/project',
       name: 'project',
       component: Project
-    } 
+    },
+    {
+      path: '/portfoliowriter',
+      name: 'portfoliowriter',
+      component: PortfolioWriter
+    },
   ]
 })
