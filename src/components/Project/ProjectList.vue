cdded73a18a11ad3875750f215040d201a7475d8
silverlyjoo <silverlyjoo@gmail.com>
2019-07-08 11:29:17 +0900

    test axios

---
+++ src/components/Project/ProjectList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-for="pjt in projects">{{pjt}}</div>
+</template>
+
+
+<script>
+
+export default {
+  name: "ProjectList",
+  data () {
+      return {
+          projects : [],
+          userNames : ['silverlyjoo']
+      };
+  },
+  getProjects(userName) {
+      axios.get(`https://lab.ssafy.com/api/v4/users/${userName}/projects`)
+      then.( (response) => {
+          this.projects = response.data
+          console.log(this.projects)
+      })
+  }
+
+};
+</script>
+
