eb6b202f4742d743ff54cfe669d9fbab660eadd5
silverlyjoo <silverlyjoo@gmail.com>
2019-07-08 11:40:45 +0900

    test axios2

--- src/components/Project/ProjectList.vue
+++ src/components/Project/ProjectList.vue
@@ -1,26 +1,33 @@
 <template>
+  <div>
     <div v-for="pjt in projects">{{pjt}}</div>
+  </div>
 </template>
 
 
 <script>
+import axios from 'axios'
 
 export default {
   name: "ProjectList",
-  data () {
-      return {
-          projects : [],
-          userNames : ['silverlyjoo']
-      };
+  data() {
+    return {
+      projects: [],
+      userNames: ["silverlyjoo"]
+    };
   },
   getProjects(userName) {
-      axios.get(`https://lab.ssafy.com/api/v4/users/${userName}/projects`)
-      then.( (response) => {
-          this.projects = response.data
-          console.log(this.projects)
-      })
+    axios.get(`https://lab.ssafy.com/api/v4/users/${userName}/projects`)
+    .then(response => {
+      this.projects.push(response.data);
+      console.log(this.projects);
+    });
+  },
+  mounted() {
+    for (let i = 0; i < this.userNames.length; i++) {
+      this.getProjects(this.userNames[i]);
+    }
   }
-
 };
 </script>
 
