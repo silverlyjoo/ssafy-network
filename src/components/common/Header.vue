9497cdd7b52e739d011c7a80415299eeae068947
silverlyjoo <silverlyjoo@gmail.com>
2019-07-09 10:24:30 +0900

    make login modal at header

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,36 +1,75 @@
 <template>
   <div>
+    <v-dialog v-model="dialog_login" max-width="500">
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Legal first name*" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field
+                  label="Legal last name*"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Email*" required></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Password*" type="password" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*" required></v-select>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-autocomplete
+                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                  label="Interests"
+                  multiple
+                ></v-autocomplete>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <small>*ndicates required field</small>
+        </v-card-text>
+
+        <v-spacer></v-spacer>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click="dialog_login = false">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click="dialog_login = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
     <v-toolbar fixed app clipped-right>
       <v-toolbar-title>
         <router-link to="/" style="text-decoration:none!important">SSAFY</router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      
+
       <v-toolbar-items>
-        <v-dialog v-model="dialog_login" width="500">
-            <template v-slot:activator="{ on }">
-              <v-btn flat v-if="item.title === 'Login'" v-on="on">Login</v-btn>
-            </template>
-            <v-card>
-              <v-card-title class="headline grey lighten-2" primary-title>로그인</v-card-title>
-              <v-card-text class="text-xs-center">로그인하세요</v-card-text>
-              <v-divider></v-divider>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" flat @click="dialog = false">accept</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
+          <v-btn flat @click.stop="dialog_login = true">Login</v-btn>
       </v-toolbar-items>
+
       <v-toolbar-items class="hidden-xs-only" v-for="item in items" :key="item.title">
         <v-btn flat @click="goPage(item.link)" exact>{{item.title}}</v-btn>
-
-        
       </v-toolbar-items>
       <span>
         <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-toolbar-side-icon>
       </span>
     </v-toolbar>
+
     <v-navigation-drawer v-model="drawer" :mini-variant="mini" fixed light temporary right>
       <v-list class="pa-1">
         <v-list-tile v-if="mini" @click.stop="mini = !mini">
