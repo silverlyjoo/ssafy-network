f5f83e7f38b0b7d645ef72fb32c5ed48c8946471
PSMiswell <smp19910413@gmail.com>
2019-07-10 17:23:39 +0900

    [박성민 포트폴리오] Portfolio Text Over Flow 방지 시도중

--- src/components/Portfolio/Portfolio.vue
+++ src/components/Portfolio/Portfolio.vue
@@ -1,58 +1,58 @@
 <template>
-    <div>
-        <v-hover>
-            <v-card
-                slot-scope="{ hover }"
-                :class="`elevation-${hover ? 12 : 2}`"
-                
-                
-            >
-                <v-img
-                :aspect-ratio="16/9"
-                :src="imgSrc"
-                ></v-img>
-                <v-card-title>
-                    <div>
-                        <div class="headline">{{title}}</div>
-                        <div class="d-flex" >
-                            <div class="ml-2 grey--text text--darken-2">
-                                <span >{{content}}</span>
-                            </div>
-                        </div>
-                    </div>
-                <v-spacer></v-spacer>
-                </v-card-title>
-            </v-card>
-        </v-hover>
-    </div>
+  <div>
+    <v-hover>
+      <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 12 : 2}`">
+        <v-img :aspect-ratio="16/9" :src="imgSrc"></v-img>
+        <v-card-title>
+          <div>
+            <div class="headline title">{{title}}</div>
+            <!-- <div class="d-flex"> -->
+            <span class="grey--text content">{{content}}</span>
+              <!-- </div> -->
+            <!-- </div> -->
+          </div>
+          <v-spacer></v-spacer>
+        </v-card-title>
+      </v-card>
+    </v-hover>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Portfolio",
-    props: {
-        id : {type:String},
-        title: {type: String},
-        content: {type: String},
-        imgSrc: { type: String },
-    },
-    data() {
-        return {
-            //
-        }
-    }
+export default {
+  name: "Portfolio",
+  props: {
+    id: { type: String },
+    title: { type: String },
+    content: { type: String },
+    imgSrc: { type: String }
+  },
+  data() {
+    return {
+      //
+    };
   }
+};
 </script>
 
 <style>
-  .cardBody {
-    white-space: nowrap;
-    overflow: hidden!important;
-    text-overflow: ellipsis!important;
-    display: -webkit-box;
-    line-height: 17px;     /* fallback */
-    -webkit-line-clamp: 1; /* number of lines to show */
-    -webkit-box-orient: vertical;
-  }
+.title {
+	overflow: hidden;
+	text-overflow: ellipsis;
+	display: -webkit-box;
+	line-height: 17px;     /* fallback */
+	max-height: 70px;      /* fallback */
+	-webkit-line-clamp: 1; /* number of lines to show */
+	-webkit-box-orient: vertical;
+}
 
+.content {
+	overflow: hidden;
+	text-overflow: ellipsis;
+	display: -webkit-box;
+	line-height: 17px;     /* fallback */
+	max-height: 70px;      /* fallback */
+	-webkit-line-clamp: 4; /* number of lines to show */
+	-webkit-box-orient: vertical;
+}
 </style>
