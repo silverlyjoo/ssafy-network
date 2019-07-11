adb44c62c1e8f6d811fc72f6529672ec39fe9637
mwh5026 <auddn9876@naver.com>
2019-07-11 15:54:33 +0900

    메인페이지 그래프 삭제

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,18 +2,15 @@
 <div>
   <ImgBanner></ImgBanner>
   <h2>메인 페이지 입니다.</h2>
-  <GitGraph></GitGraph>
 </div>
 </template>
 
 <script>
 import ImgBanner from "@/components/common/ImgBanner.vue";
-import GitGraph from "@/components/Project/GitGraph.vue";
 
 export default {
   components: {
-    ImgBanner,
-    GitGraph
+    ImgBanner
   }
 }
 </script>
