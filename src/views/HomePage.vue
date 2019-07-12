6b277656431d4d1c17589df8ac783765051aaacb
silverlyjoo <silverlyjoo@gmail.com>
2019-07-12 10:08:25 +0900

    Homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="mainPage">
-  <div class="mainLogo">WM project</div>
-  <GitGraph></GitGraph>
-</div>
+  <div class="mainPage">
+    <div class="mainLogo Absolute-Center">WM project</div>
+    <router-link to="/project" class="projectlink linktext">project</router-link>
+  </div>
 </template>
 
 <script>
@@ -10,30 +10,46 @@ import ImgBanner from "@/components/common/ImgBanner.vue";
 
 export default {
   components: {
-    ImgBanner,
-    GitGraph
+    ImgBanner
   },
   methods: {
     pageLog() {
-      console.log(this.$router.history.current)
+      console.log(this.$router.history.current);
     }
   },
   mounted() {
-
-    this.pageLog()
+    this.pageLog();
   }
-}
+};
 </script>
 <style>
 .mainPage {
-  background-color:rgb(46, 46, 46);
+  background-color: rgb(46, 46, 46);
   position: absolute;
   width: 100vw;
   height: 100vh;
+  text-align: center;
+  
 }
 .mainLogo {
-  color: rgb(163, 194, 240);
-  font-size: 60px;
-  font-family: 'Roboto Mono', monospace;
+  color: rgb(82, 125, 189);
+  font-size: 7vw;
+  font-family: "Roboto Slab", serif;
+  font-weight: 300;
+  /* line-height: 50vw */
+}
+.Absolute-Center {
+  margin-top: 50vh;
+}
+.linktext {
+  color: white;
+  text-decoration: none;
+  font-family: 'Roboto';
+  font-size: 5vw;
+  font-weight: 200;
+}
+.projectlink {
+  position: absolute;
+  
 }
 </style>
