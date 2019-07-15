97820ed9dfa1729a377114d5651796a3a00cbabd
김경태 <kkt3526@naver.com>
2019-07-15 10:13:41 +0900

    DB 연결 성공

--- ssafy-network/back/app.js
+++ ssafy-network/back/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var mongoose = require('mongoose');
+var db = require('./db');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+db();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
