5f912450fe04000347ec95a240c52be777e43214
silverlyjoo <silverlyjoo@gmail.com>
2019-07-15 11:15:17 +0900

    add vuetify

--- ssafy-network/front/src/App.vue
+++ ssafy-network/front/src/App.vue
@@ -1,23 +1,38 @@
 <template>
   <div id="app">
-    <Header></Header>
-    <router-view></router-view>
-    <Footer></Footer>
+    <v-layout>
+      <v-flex xs2 class="Nav">
+        <Nav></Nav>
+      </v-flex>
+
+      <v-flex xs10>
+        <Header></Header>
+        <router-view></router-view>
+        <Footer></Footer>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
+import Nav from "@/components/common/Nav";
 
 export default {
   name: "App",
   components: {
     Header,
-    Footer
+    Footer,
+    Nav
   },
   data() {
     return {};
   }
 };
 </script>
+<style>
+.Nav {
+  height: 100vh;
+}
+</style>
