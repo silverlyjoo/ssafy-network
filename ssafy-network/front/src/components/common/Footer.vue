1b87329272c017e42c1eeb182384d8385bae3960
mwh5026 <auddn9876@naver.com>
2019-07-15 13:48:30 +0900

    header 수정중

--- ssafy-network/front/src/components/common/Footer.vue
+++ ssafy-network/front/src/components/common/Footer.vue
@@ -1,5 +1,45 @@
 <template>
-  <div>
-    <v-footer>footer</v-footer>
-  </div>
+  <v-footer
+    height="auto"
+    color="indigo lighten-1"
+  >
+    <v-layout
+      justify-center
+      row
+      wrap
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        flat
+        round
+      >
+        {{ link }}
+      </v-btn>
+      <v-flex
+        indigo
+        lighten-2
+        py-3
+        text-xs-center
+        white--text
+        xs12
+      >
+        &copy;2019 — <strong>SSAFY(WM 팀)</strong>
+      </v-flex>
+    </v-layout>
+  </v-footer>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Home',
+        'About Us',
+        'Services',
+        'Contact Us'
+      ]
+    })
+  }
+</script>
--- ssafy-network/front/src/components/common/Header.vue
+++ ssafy-network/front/src/components/common/Header.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>
-    <v-toolbar>header</v-toolbar>
-  </div>
+  <v-toolbar color="indigo lighten-1">
+    <v-toolbar-title class="white--text">WeMe</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+  <v-toolbar-items>
+    <v-btn icon>
+      <v-icon color="white">dashboard</v-icon>
+    </v-btn>
+    <v-menu bottom origin="center center" transition="scale-transition">
+      <template v-slot:activator="{ on }">
+        <v-btn icon  v-on="on">
+          <v-icon color="white">notifications</v-icon>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-tile v-for="(item, i) in items" :key="i" @click="goPage()">
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+    <v-btn icon>
+      <v-icon color="white">person</v-icon>
+    </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { title: 'Click Me 1' },
+        { title: 'Click Me 2' },
+        { title: 'Click Me 3' },
+        { title: 'Click Me 4' }
+      ]
+    })
+  }
+</script>
