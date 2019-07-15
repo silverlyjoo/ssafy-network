84e688028aba5007e35fd224a56b31e6a6b2b16f
mwh5026 <auddn9876@naver.com>
2019-07-15 11:17:15 +0900

    merge

--- ssafy-network/front/src/components/note/Note.vue
+++ ssafy-network/front/src/components/note/Note.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an Note</h1>
-  </div>
+<div>
+    <v-layout wrap row>
+      <v-flex xs12 sm6>
+        <editor v-model="editorText"/>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <viewer :value="editorText" />
+      </v-flex>
+      <v-flex xs12>
+        <span>{{editorText}}</span>
+      </v-flex>
+    </v-layout>
+</div>
 </template>
 
 <script>
 export default {
   name: 'Note',
+  data(){
+    return {
+      editorText:''
+    }
+  }
 }
 </script>
--- ssafy-network/front/src/main.js
+++ ssafy-network/front/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import 'tui-editor/dist/tui-editor.css'
+import 'tui-editor/dist/tui-editor-contents.css'
+import 'codemirror/lib/codemirror.css'
+import { Editor, Viewer } from '@toast-ui/vue-editor'
+
+Vue.component('editor', Editor)
+Vue.component('viewer', Viewer)
 
 Vue.config.productionTip = false
 
