cd89c1c32c262d05202e83c6766a3b6dd6e7b87a
mwh5026 <auddn9876@naver.com>
2019-07-15 10:18:05 +0900

    merge

--- ssafy-network/back/app.js
+++ ssafy-network/back/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var db = require('./db');
+var mongoose = require('mongoose');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-db();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
