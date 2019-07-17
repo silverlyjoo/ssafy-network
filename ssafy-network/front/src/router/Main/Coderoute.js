7d255ada7b1d22cdaec7699aee9cb71835768968
PSMiswell <smp19910413@gmail.com>
2019-07-17 17:06:56 +0900

    [박성민 Code] 게시판 구현중 - 코드는 짰으나 페이지에 안 뜸

--- ssafy-network/front/src/router/Main/Coderoute.js
+++ ssafy-network/front/src/router/Main/Coderoute.js
@@ -1,15 +1,15 @@
 import CodePage from '@/views/CodePage.vue'
-// import CodeWriter from '@components/code/CodeWriter.vue'
+import CodeWriter from '@/components/code/CodeWriter.vue'
 
 export default {
     path: '/code',
     name: 'code',
     component: CodePage,
     children: [
-    //     {
-    //         path: '/write',
-    //         name: 'codewrite',
-    //         component: CodeWriter,
-    //     },
+        {
+            path: '/write',
+            name: 'codewrite',
+            component: CodeWriter,
+        },
     ]
 }
