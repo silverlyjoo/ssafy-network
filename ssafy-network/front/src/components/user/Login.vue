43455add85603913197b234f75f0aed2f4ffed97
mwh5026 <auddn9876@naver.com>
2019-07-18 17:35:33 +0900

    login DB 연결

--- ssafy-network/front/src/components/user/Login.vue
+++ ssafy-network/front/src/components/user/Login.vue
@@ -1,22 +1,20 @@
 <template>
-  <v-layout  justify-center xs10 sm8>
+  <v-layout justify-center xs10 sm8>
     <v-card style="width:60%">
       <v-container>
         <v-layout>
           <v-flex>
-            <v-text-field label="ID"></v-text-field>
+            <v-text-field label="ID" v-model="id" ref="id"></v-text-field>
           </v-flex>
         </v-layout>
         <v-layout>
           <v-flex>
             <v-text-field
-              v-model="password"
+              v-model="pwd"
               :append-icon="show1 ? 'visibility' : 'visibility_off'"
-              :rules="[rules.required, rules.min]"
               :type="show1 ? 'text' : 'password'"
               name="input-10-1"
               label="PASSWORD"
-              hint="At least 8 characters"
               counter
               @click:append="show1 = !show1"
             ></v-text-field>
@@ -24,8 +22,8 @@
         </v-layout>
 
         <v-card-actions>
-          <v-btn flat color="grey">Submit</v-btn>
-          <v-btn flat color="grey">Cancle</v-btn>
+          <v-btn flat color="primary" @click="login">로그인</v-btn>
+          <v-btn flat color="primary">돌아가기</v-btn>
         </v-card-actions>
       </v-container>
     </v-card>
@@ -37,13 +35,42 @@ export default {
   data() {
     return {
       show1: false,
-      password: "",
-      rules: {
-        required: value => !!value || "Required.",
-        min: v => v.length >= 8 || "Min 8 characters",
-        emailMatch: () => "The email and password you entered don't match"
-      }
+      id: "",
+      pwd: ""
     };
+  },
+  methods: {
+    login() {
+      fetch(this.$store.state.dbserver + "/users/login", {
+        method: "POST",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          id: this.id,
+          pwd: this.pwd
+        })
+      })
+        .then(res => res.json())
+        .then(data => {
+          if (data.result) {
+            this.$store.state.login = true;
+            this.$session.set("user" , this.id);
+            this.id = "";
+            this.pwd = "";
+            console.log("로그인 성공!!!");
+            this.$router.push("/index");
+          } else {
+            alert("로그인 실패...");
+            this.id = "";
+            this.pwd = "";
+            this.$refs.id.focus();
+          }
+        })
+        .catch(error => console.log(error))
+        .finally();
+    }
   }
 };
 </script>
