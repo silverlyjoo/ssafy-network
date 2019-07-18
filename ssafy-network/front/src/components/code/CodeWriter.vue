9dce18d4faa9e1b771346e0ed2a53e8198f80d93
PSMiswell <smp19910413@gmail.com>
2019-07-18 16:31:11 +0900

    [박성민 Code] Code 게시판 수정중

--- ssafy-network/front/src/components/code/CodeWriter.vue
+++ ssafy-network/front/src/components/code/CodeWriter.vue
@@ -1,132 +1,117 @@
 <template>
-  <v-card>
-    <v-card-media>
-      <div class="vue">
-        <div class="codemirror">
-          <!-- codemirror -->
-          <codemirror v-model="code" 
-                      :options="cmOption"
-                      @cursorActivity="onCmCursorActivity"
-                      @ready="onCmReady"
-                      @focus="onCmFocus"
-                      @blur="onCmBlur">
-          </codemirror>
-        </div>
-        <pre class="pre">{{ code }}</pre>
+  <md-card>
+    <md-card-media>
+      <div class="codemirror">
+        <!-- codemirror -->
+        <codemirror v-model="code" :options="cmOption"></codemirror>
       </div>
-    </v-card-media>
-  </v-card>
+    </md-card-media>
+  </md-card>
 </template>
 
 <script>
   // language
-  import 'codemirror/mode/vue/vue.js'
+  import 'codemirror/mode/javascript/javascript.js'
   // theme css
-  import 'codemirror/theme/base16-dark.css'
-  // active-line.js
-  import 'codemirror/addon/selection/active-line.js'
+  import 'codemirror/theme/monokai.css'
+  // require active-line.js
+  import'codemirror/addon/selection/active-line.js'
   // styleSelectedText
-  import 'codemirror/addon/selection/mark-selection.js'
-  import 'codemirror/addon/search/searchcursor.js'
+  import'codemirror/addon/selection/mark-selection.js'
+  import'codemirror/addon/search/searchcursor.js'
+  // hint
+  import'codemirror/addon/hint/show-hint.js'
+  import'codemirror/addon/hint/show-hint.css'
+  import'codemirror/addon/hint/javascript-hint.js'
+  import'codemirror/addon/selection/active-line.js'
   // highlightSelectionMatches
-  import 'codemirror/addon/scroll/annotatescrollbar.js'
-  import 'codemirror/addon/search/matchesonscrollbar.js'
-  import 'codemirror/addon/search/searchcursor.js'
-  import 'codemirror/addon/search/match-highlighter.js'
+  import'codemirror/addon/scroll/annotatescrollbar.js'
+  import'codemirror/addon/search/matchesonscrollbar.js'
+  import'codemirror/addon/search/searchcursor.js'
+  import'codemirror/addon/search/match-highlighter.js'
   // keyMap
-  import 'codemirror/mode/clike/clike.js'
-  import 'codemirror/addon/edit/matchbrackets.js'
-  import 'codemirror/addon/comment/comment.js'
-  import 'codemirror/addon/dialog/dialog.js'
-  import 'codemirror/addon/dialog/dialog.css'
-  import 'codemirror/addon/search/searchcursor.js'
-  import 'codemirror/addon/search/search.js'
-  import 'codemirror/keymap/sublime.js'
+  import'codemirror/mode/clike/clike.js'
+  import'codemirror/addon/edit/matchbrackets.js'
+  import'codemirror/addon/comment/comment.js'
+  import'codemirror/addon/dialog/dialog.js'
+  import'codemirror/addon/dialog/dialog.css'
+  import'codemirror/addon/search/searchcursor.js'
+  import'codemirror/addon/search/search.js'
+  import'codemirror/keymap/sublime.js'
   // foldGutter
-  import 'codemirror/addon/fold/foldgutter.css'
-  import 'codemirror/addon/fold/brace-fold.js'
-  import 'codemirror/addon/fold/comment-fold.js'
-  import 'codemirror/addon/fold/foldcode.js'
-  import 'codemirror/addon/fold/foldgutter.js'
-  import 'codemirror/addon/fold/indent-fold.js'
-  import 'codemirror/addon/fold/markdown-fold.js'
-  import 'codemirror/addon/fold/xml-fold.js'
-  export default {
-    data() {
-      const code = ''
-`<template>
-  <h1>Hello World!</h1>
-  <codemirror v-model="code" :options="cmOption"></codemirror>
-</template>
-<script>
-  // import 'some-codemirror-resource'
+  import'codemirror/addon/fold/foldgutter.css'
+  import'codemirror/addon/fold/brace-fold.js'
+  import'codemirror/addon/fold/comment-fold.js'
+  import'codemirror/addon/fold/foldcode.js'
+  import'codemirror/addon/fold/foldgutter.js'
+  import'codemirror/addon/fold/indent-fold.js'
+  import'codemirror/addon/fold/markdown-fold.js'
+  import'codemirror/addon/fold/xml-fold.js'
+  
   export default {
     data() {
       return {
-        code: 'const A = 10',
+        code :
+`import someResource from 'codemirror/some-resource'
+export default {
+  data () {
+    // 这是一个包含、代码提示、折叠、选中、sublime模式...的编辑器
+    // 按下Ctrl键可以体验代码提示
+    // 可以尝试sublime下的快捷键操作
+    return {
+      exampleCode: 'const a = 10',
+      cmOption: {
+        tabSize: 4,
+        styleActiveLine: true,
+        lineNumbers: true,
+        line: true,
+        mode: 'text/javascript',
+        lineWrapping: true,
+        theme: 'default'
+      }
+    }
+  },
+  components: examples
+}`,
         cmOption: {
           tabSize: 4,
-          styleActiveLine: true,
+          styleActiveLine: false,
           lineNumbers: true,
+          styleSelectedText: false,
           line: true,
           foldGutter: true,
-          styleSelectedText: true,
+          gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+          highlightSelectionMatches: { showToken: /\w/, annotateScrollbar: true },
           mode: 'text/javascript',
+          // hint.js options
+          hintOptions:{
+            // 当匹配只有一项的时候是否自动补全
+            completeSingle: false
+          },
+          //快捷键 可提供三种模式 sublime、emacs、vim
           keyMap: "sublime",
           matchBrackets: true,
           showCursorWhenSelecting: true,
           theme: "monokai",
-          extraKeys: { "Ctrl": "autocomplete" },
-          hintOptions:{
-            completeSingle: false
-          }
-        }
-      }
-    }
-  }
-<\/script>
-<style lang="scss">
-  @import './sass/mixins';
-  @import './sass/variables';
-  main {
-    position: relative;
-  }
-</style>`
-      return {
-        code,
-        cmOption: {
-          tabSize: 4,
-          foldGutter: true,
-          styleActiveLine: true,
-          lineNumbers: true,
-          line: true,
-          keyMap: "sublime",
-          mode: 'text/x-vue',
-          theme: 'base16-dark',
-          extraKeys: {
-            'F11'(cm) {
-              cm.setOption("fullScreen", !cm.getOption("fullScreen"))
-            },
-            'Esc'(cm) {
-              if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false)
-            }
-          }
+          extraKeys: { "Ctrl": "autocomplete" }
         }
       }
     },
-    methods: {
-      onCmCursorActivity(codemirror) {
-        console.log('onCmCursorActivity', codemirror)
-      },
-      onCmReady(codemirror) {
-        console.log('onCmReady', codemirror)
-      },
-      onCmFocus(codemirror) {
-        console.log('onCmFocus', codemirror)
-      },
-      onCmBlur(codemirror) {
-        console.log('onCmBlur', codemirror)
-      }
+    mounted() {
+      setTimeout(() => {
+        this.styleSelectedText =  true,
+        this.cmOption.styleActiveLine = true
+      }, 1800)
     }
   }
 </script>
+
+<style>
+  .CodeMirror-focused .cm-matchhighlight {
+    background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAFklEQVQI12NgYGBgkKzc8x9CMDAwAAAmhwSbidEoSQAAAABJRU5ErkJggg==);
+    background-position: bottom;
+    background-repeat: repeat-x;
+  }
+  .cm-matchhighlight {background-color: lightgreen}
+  .CodeMirror-selection-highlight-scrollbar {background-color: green}
+</style>
