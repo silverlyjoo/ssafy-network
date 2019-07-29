5e23e0542f1fafed9bbf98e1d71b153f815af6fe
mwh5026 <auddn9876@naver.com>
2019-07-29 12:09:08 +0900

    svg loader 복원

--- ssafy-network/front/vue.config.js
+++ ssafy-network/front/vue.config.js
@@ -1,27 +1,8 @@
 module.exports = {
-    pluginOptions: {
-        svgSprite: {
-            /*
-             * The directory containing your SVG files.
-             */
-            dir: 'src/assets/icons',
-            /*
-             * The reqex that will be used for the Webpack rule.
-             */
-            test: /\.(svg)(\?.*)?$/,
-            /*
-             * @see https://github.com/kisenka/svg-sprite-loader#configuration
-             */
-            loaderOptions: {
-                extract: true,
-                spriteFilename: 'img/icons.[hash:8].svg' // or 'img/icons.svg' if filenameHashing == false
-            },
-            /*
-             * @see https://github.com/kisenka/svg-sprite-loader#configuration
-             */
-            pluginOptions: {
-                plainSprite: true
-            }    
-        }    
-    }    
+    chainWebpack: config => {
+        config.module
+          .rule('svg')
+          .use('file-loader')
+          .loader('svg-sprite-loader')
+      }
 }
