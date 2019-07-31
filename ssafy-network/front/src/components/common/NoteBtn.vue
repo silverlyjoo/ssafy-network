e08969292280d6c4eefa7b9f3322ff9d0325bec4
mwh5026 <auddn9876@naver.com>
2019-07-31 16:16:11 +0900

    note root 파일,폴더 추가기능 구현

--- ssafy-network/front/src/components/common/NoteBtn.vue
+++ ssafy-network/front/src/components/common/NoteBtn.vue
@@ -1,13 +1,43 @@
 <template>
   <div class="navBtn mb-3">
     <v-layout align-center class="pa-2" @click="goNote()">
-      <v-flex xs7 text-xs-center>
-        
+      <v-flex xs7 text-xs-center @click="showRoot=true">
         <span class="navtext navtcolor">NOTE</span>
       </v-flex>
       <v-flex v-if="overlay">
-        <v-progress-circular indeterminate color="white" 
-             size ="15" ></v-progress-circular>
+        <v-progress-circular indeterminate color="white" size="15"></v-progress-circular>
+      </v-flex>
+      <v-flex justify-end text-xs-right v-if="showRoot">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              flat
+              v-on="on"
+              @click="addRootNoteOpen()"
+              small
+              class="ma-0 pa-0"
+              style="min-width:10px!important;"
+            >
+              <v-icon small>note_add</v-icon>
+            </v-btn>
+          </template>
+          <span>파일 추가</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              v-on="on"
+              flat
+              @click="addRootFolderOpen()"
+              small
+              class="ma-0 pa-0"
+              style="min-width:10px!important;"
+            >
+              <v-icon small>add_box</v-icon>
+            </v-btn>
+          </template>
+          <span>폴더 추가</span>
+        </v-tooltip>
       </v-flex>
     </v-layout>
     <v-treeview
@@ -22,7 +52,8 @@
     >
       <template v-slot:prepend="{item, open,selected}">
         <v-btn flat class="ma-0 pa-0" style="min-width:30px!important;">
-          <v-icon class="iconn"
+          <v-icon
+            class="iconn"
             v-if="item.file == 'folder'"
             open-on-click
           >{{ open ? 'mdi-folder-open' : 'mdi-folder' }}</v-icon>
@@ -149,6 +180,56 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+    <v-dialog v-model="showRootNote" max-width="300">
+      <v-card class="pa-2">
+        <v-card-title class="headline justify-center">파일 추가</v-card-title>
+        <v-card-actions class="text-xs-center">
+          <v-container>
+            <v-layout wrap>
+              <v-flex>
+                <v-text-field
+                  label="파일 제목"
+                  v-model="NoteTitle"
+                  v-validate="'required|min:2'"
+                  data-vv-name="NoteTitle"
+                  data-vv-scope="NoteTitle"
+                  :error-messages="errors.collect('NoteTitle')"
+                ></v-text-field>
+              </v-flex>
+              <v-flex>
+                <v-btn color="green darken-1" flat="flat" @click="closeRoot()">취소</v-btn>
+                <v-btn color="green darken-1" flat="flat" @click="addRootNote()">추가</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="showRootFolder" max-width="300">
+      <v-card class="pa-2">
+        <v-card-title class="headline justify-center">폴더 추가</v-card-title>
+        <v-card-actions class="text-xs-center">
+          <v-container>
+            <v-layout wrap>
+              <v-flex>
+                <v-text-field
+                  label="폴더 제목"
+                  v-model="FolderTitle"
+                  v-validate="'required|min:2'"
+                  data-vv-name="FolderTitle"
+                  data-vv-scope="FolderTitle"
+                  :error-messages="errors.collect('FolderTitle')"
+                ></v-text-field>
+              </v-flex>
+              <v-flex>
+                <v-btn color="green darken-1" flat="flat" @click="closeRoot()">취소</v-btn>
+                <v-btn color="green darken-1" flat="flat" @click="addRootFolder()">추가</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-dialog v-model="showFolderEdit" max-width="300">
       <v-card class="pa-2">
         <v-card-title class="headline justify-center">폴더 이름 수정</v-card-title>
@@ -184,7 +265,6 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-      
   </div>
 </template>
 
@@ -194,9 +274,13 @@ import { mapState } from "vuex";
 export default {
   data() {
     return {
+      showRoot:false,
+      rootid: "",
       overlay: false,
       showNote: false,
+      showRootNote: false,
       showFolder: false,
+      showRootFolder: false,
       showDelete: false,
       showFolderEdit: false,
       seleteItem: "",
@@ -220,34 +304,205 @@ export default {
     };
   },
   methods: {
-    FolderEdit(item){
+    closeRoot() {
+      this.showRootNote = false;
+      this.showRootFolder = false;
+      this.rootid = "";
+      this.closeForm();
+    },
+    addRootNoteOpen() {
+      this.showRootNote = true;
+      fetch(
+        this.$store.state.dbserver +
+          "/trees/super/" +
+          this.$session.get("id") +
+          "/" +
+          this.$session.get("token"),
+        {
+          method: "GET",
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json"
+          }
+        }
+      )
+        .then(res => res.json())
+        .then(data => {
+          if (data.result == false) {
+            alert("root 폴더가 없습니다...");
+          } else {
+            this.rootid = data._id;
+          }
+        });
+    },
+    addRootNote() {
+       this.$validator.validateAll("NoteTitle").then(res => {
+        if (!res) {
+          alert("값이 유효한지 확인해 주세요.");
+        } else {
+          // 같은 폴더내에 이름이 같은 파일이 존재하는지 체크
+          const pid = this.rootid;
+          const title = this.NoteTitle;
+          const tokenid = this.$session.get("token");
+          fetch(
+            this.$store.state.dbserver +
+              "/trees/txt/" +
+              pid +
+              "/" +
+              title +
+              "/" +
+              tokenid,
+            {
+              method: "GET",
+              headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json"
+              }
+            }
+          )
+            .then(res => res.json())
+            .then(data => {
+              if (data.result == false) {
+                // 존재 하지 않는다면 post 로 추가한다.
+                fetch(this.$store.state.dbserver + "/trees/txt", {
+                  method: "POST",
+                  headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json"
+                  },
+                  body: JSON.stringify({
+                    token: tokenid,
+                    parent_id: pid,
+                    name: title
+                  })
+                })
+                  .then(res => res.json())
+                  .then(data => {
+                    if (data.result == true) {
+                      // 만약 추가가 성공한다면 그 id 값을 조회해서 writeForm 으로 보내준다.
+                      fetch(
+                        this.$store.state.dbserver +
+                          "/trees/txt/" +
+                          pid +
+                          "/" +
+                          title +
+                          "/" +
+                          tokenid,
+                        {
+                          method: "GET",
+                          headers: {
+                            "Access-Control-Allow-Origin": "*",
+                            "Content-Type": "application/json"
+                          }
+                        }
+                      )
+                        .then(res => res.json())
+                        .then(data => {
+                          if (data.result == false) {
+                            alert("추가를 실패하였습니다...");
+                          } else {
+                            this.$router.push({
+                              name: "notewrite",
+                              params: { title: data.name, _id: data._id }
+                            });
+                          }
+                        });
+                    } else {
+                      alert("실패");
+                    }
+                  });
+              } else {
+                alert("이미 존재하는 파일입니다. (실패...)");
+              }
+              this.closeRoot();
+            });
+        }
+      });
+    },
+    addRootFolderOpen() {
+      this.showRootFolder = true;
+      fetch(
+        this.$store.state.dbserver +
+          "/trees/super/" +
+          this.$session.get("id") +
+          "/" +
+          this.$session.get("token"),
+        {
+          method: "GET",
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json"
+          }
+        }
+      )
+        .then(res => res.json())
+        .then(data => {
+          if (data.result == false) {
+            alert("root 폴더가 없습니다...");
+          } else {
+            this.rootid = data._id;
+          }
+        });
+    },
+    addRootFolder() {
+      this.$validator.validateAll("FolderTitle").then(res => {
+        if (!res) {
+          alert("값이 유효한지 확인해 주세요.");
+        } else {
+      fetch(this.$store.state.dbserver + "/trees/folder", {
+                  method: "POST",
+                  headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json"
+                  },
+                  body: JSON.stringify({
+                    token: this.$session.get("token"),
+                    parent_id: this.rootid,
+                    name: this.FolderTitle
+                  })
+                })
+                  .then(res => res.json())
+                  .then(data => {
+                    if(data.result == true){
+                      alert("폴더 추가 성공!");
+                      
+                    }else{
+                      alert("폴더 추가 실패...");
+                    }
+                    this.closeRoot();
+                  });
+        }
+      });
+    },
+    FolderEdit(item) {
       this.seleteItem = item;
       this.FolderTitle = item.name;
       this.showFolderEdit = true;
     },
-    FolderUpdate(){
+    FolderUpdate() {
       fetch(this.$store.state.dbserver + "/trees/folder", {
-            method: "PUT",
-            headers: {
-              "Access-Control-Allow-Origin": "*",
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-              _id : this.seleteItem._id,
-              token : this.$session.get("token"),
-              name : this.FolderTitle,
-            })
-          })
-            .then(res => res.json())
-            .then(data => {
-                if(data.result == true){
-                    alert("폴더 이름 수정 성공");
-                }else{
-                    alert("폴더 이름 수정 실패..");
-                }
-                this.FolderEditClose();
-            });
-    },FolderEditClose(){
+        method: "PUT",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          _id: this.seleteItem._id,
+          token: this.$session.get("token"),
+          name: this.FolderTitle
+        })
+      })
+        .then(res => res.json())
+        .then(data => {
+          if (data.result == true) {
+            alert("폴더 이름 수정 성공");
+          } else {
+            alert("폴더 이름 수정 실패..");
+          }
+          this.FolderEditClose();
+        });
+    },
+    FolderEditClose() {
       this.showFolderEdit = false;
       this.FolderTitle = "";
       this.closeForm();
@@ -288,6 +543,7 @@ export default {
       this.$router.push("/note/calendar");
       this.click = true;
       this.$store.state.notetreefoldflag = true;
+      this.getItems();
     },
     addNote() {
       this.$validator.validateAll("NoteTitle").then(res => {
@@ -468,10 +724,9 @@ export default {
     }
   },
   mounted() {
-    this.getItems();
+    
   },
-  computed:
-    mapState(["NoteCheck" , "notetreefoldflag"]),
+  computed: mapState(["NoteCheck", "notetreefoldflag"]),
   watch: {
     NoteCheck(to, from) {
       if (from == false && to == true) {
@@ -479,18 +734,17 @@ export default {
         this.$store.state.NoteCheck = false;
       }
     },
-    notetreefoldflag(to , from){
+    notetreefoldflag(to, from) {
       this.click = this.$store.state.notetreefoldflag;
+      this.showRoot = this.click;
     },
-    overlay(to,from){
-      if(to == true && from == false){
-          setTimeout(() => {
+    overlay(to, from) {
+      if (to == true && from == false) {
+        setTimeout(() => {
           this.overlay = false;
         }, 3000);
       }
-      
     }
-
   }
 };
 </script>
@@ -505,11 +759,11 @@ export default {
   color: white;
   font-size: 16px;
   font-weight: 100;
-  font-family:'Nanum Gothic Coding';
+  font-family: "Nanum Gothic Coding";
 }
 .treecss {
   padding: 10px;
   background-color: rgb(117, 117, 117);
-  border-radius : 10px;
+  border-radius: 10px;
 }
 </style>
