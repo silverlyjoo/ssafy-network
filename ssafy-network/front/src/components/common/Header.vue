e3855532b552ff9a091c96e7317df4043895ea59
mwh5026 <auddn9876@naver.com>
2019-08-01 09:50:54 +0900

    header 라우팅  수정

--- ssafy-network/front/src/components/common/Header.vue
+++ ssafy-network/front/src/components/common/Header.vue
@@ -3,12 +3,41 @@
     <v-toolbar-title class="white--text title">SSAFY WM</v-toolbar-title>
     <v-spacer/>
     <v-toolbar-items>
-      <v-btn icon >
+      <v-btn icon @click="goCalendar()">
       <v-icon color="white">dashboard</v-icon>
     </v-btn>
-    <v-btn icon>
+    <v-menu
+          bottom
+          left
+          content-class="dropdown-menu"
+          offset-y
+          transition="slide-y-transition"
+          >
+    <v-btn icon slot="activator">
+      <v-badge
+              color="red"
+              overlap
+            >
+              <template slot="badge">
+                {{ notifications.length }}
+              </template>
       <v-icon color="white">notifications</v-icon>
+            </v-badge>
     </v-btn>
+    <v-card>
+            <v-list dense>
+              <v-list-tile
+                v-for="notification in notifications"
+                :key="notification"
+                @click="onClick"
+              >
+                <v-list-tile-title
+                  v-text="notification"
+                />
+              </v-list-tile>
+            </v-list>
+          </v-card>
+    </v-menu>
     <v-btn icon>
       <v-icon color="white">person</v-icon>
     </v-btn>
@@ -22,18 +51,24 @@
 <script>
   export default {
     data: () => ({
-      items: [
-        { title: 'Click Me 1' },
-        { title: 'Click Me 2' },
-        { title: 'Click Me 3' },
-        { title: 'Click Me 4' }
-      ]
+      notifications: [
+      '메세지 11',
+      '메세지 22',
+      '메세지 33',
+      '메세지 44',
+      '메세지 55'
+    ],
     }),
     methods:{
 logout(){
       this.$store.state.login = false;
       this.$session.clear();
       this.$router.push('/');
+    },
+    goCalendar(){
+      this.$router.push("/note/calendar");
+    },onClick(){
+      alert("1!");
     }
     }
     
