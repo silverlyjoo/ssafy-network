23b50a9068e9fdf865f887294a5e5387f762c259
mwh5026 <auddn9876@naver.com>
2019-08-05 17:06:28 +0900

    editor 선택 추가중

---
+++ ssafy-network/front/src/components/note/CodeEditor.vue
@@ -0,0 +1,17 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+  name: "CodeEditor",
+  props: {
+    _id: { type: String },
+    title: { type: String }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
--- ssafy-network/front/src/components/note/NoteList.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <v-container>
-        <v-layout>
-            <v-flex>
-                
-            </v-flex>
-        </v-layout>
-    </v-container>
-</template>
