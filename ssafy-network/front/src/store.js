dc8c3cf5924d3e7142d0f88bd7f7bdecd6046e17
silverlyjoo <silverlyjoo@gmail.com>
2019-08-08 17:48:31 +0900

    notice finish

--- ssafy-network/front/src/store.js
+++ ssafy-network/front/src/store.js
@@ -1,25 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import notice from '@/store/notice.js'
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     login: false,
-    navFoldFlag : true,
-    dbserver : 'http://192.168.31.55:3000',
-    chatserver : 'http://192.168.31.55:3000',
+    navFoldFlag: true,
+    dbserver: 'http://192.168.31.55:3000',
+    chatserver: 'http://192.168.31.55:3000',
     CalendarCheck: false,
     NoteCheck: false,
-    chatmsg : [],
+    chatmsg: [],
     notetreefoldflag: false,
-    heightflag:false,
-    commentflag : false,
+    heightflag: false,
+    commentflag: false,
   },
   mutations: {
 
   },
   actions: {
 
+  },
+  modules: {
+    notice
   }
 })
